c202b3396b8234342b7731e8b92a156cd2c0ddb1
ajryan <none@none.com>
2010-07-13 12:51:58 +0000

    Fix assembly info and executable name

--- Trunk/TestStepsEditor/Properties/AssemblyInfo.cs
+++ Trunk/TestStepsEditor/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TesStepstEditor")]
+[assembly: AssemblyTitle("TestStepsEditor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lonza")]
